03395eec135d01754bbdf21e1343fdf67eb9001f
Charles Li <charlesli@crc-dot1x-nat-10-239-145-197.bu.edu>
2023-09-19 20:52:49 -0400

    1-7

--- exams/quiz1/MySolution/quiz1-7.ml
+++ exams/quiz1/MySolution/quiz1-7.ml
@@ -11,11 +11,20 @@
 
 let isPrime(n) =
   let test(i:int): bool = 
-    if n = i then 
+    if n = (i+1) then 
       true
-    else if n mod i = 0 then
-      false
+    else if (i >= 2) && (n mod (i+1) == 0) then
+      false else true
   in
   if n < 2 then false else int1_forall(n)(test)
 
+;;
+
 (* ************************************************ *)
+
+isPrime(2);;
+isPrime(5);; 
+isPrime(11);;
+isPrime(20);;
+isPrime(100);;
+isPrime(4);;
