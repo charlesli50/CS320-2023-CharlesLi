e4b9fda08e1f0594fa1972b5dae90319a74e637b
Charles Li <charlesli@crc-dot1x-nat-10-239-70-221.bu.edu>
2023-10-05 16:07:49 -0400

    2-5

--- exams/quiz2/MySolution/OCaml/quiz2-5.ml
+++ exams/quiz2/MySolution/OCaml/quiz2-5.ml
@@ -10,5 +10,16 @@ is empty, raise the Empty exception
 
 (* ************************************************ *)
 
+#use "./../../../../classlib/OCaml/MyOCaml.ml";;
+
 exception Empty
-let list_last(xs: 'a list): 'a = ....
+let list_last(xs: 'a list): 'a = 
+match xs with 
+| [] -> raise Empty
+| xs -> list_foldright(xs)(-1)(fun x0 acc -> 
+    if acc = -1 then x0 else acc
+  )
+;;
+
+let s = list_last([5;2;3;5;4;5]);;
+let st = list_last([5;4;3;2;1]);;
