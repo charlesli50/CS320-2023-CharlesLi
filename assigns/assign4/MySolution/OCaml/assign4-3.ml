35baf0afcc7de47eb9c2bede2c2eda7590e55026
Charles Li <charlesli@crc-dot1x-nat-10-239-70-221.bu.edu>
2023-10-16 14:30:58 -0400

    4-3

--- assigns/assign4/MySolution/OCaml/assign4-3.ml
+++ assigns/assign4/MySolution/OCaml/assign4-3.ml
@@ -10,9 +10,59 @@ type 'a gtree =
 
 (* ****** ****** *)
 
+let
+fchildren
+(x0: 'a gtree): 'a gtree list =
+match x0 with
+| GTnil -> [] | GTcons(_, xs) -> xs
+
+(* ****** ****** *)
+
+let rec
+gtree_dfs
+( nxs
+: 'b list)
+( fchildren
+: 'b -> 'b list): 'b stream = fun() ->
+(
+match nxs with
+  [] -> StrNil
+| nx1 :: nxs ->
+  StrCons(nx1, gtree_dfs(fchildren(nx1) @ nxs)(fchildren))
+)
+;;
+
+let rec streamize(resStream) = fun() ->
+  match resStream() with
+  | StrNil -> StrNil
+  | StrCons(x, xs) ->
+    match x with
+    |GTnil -> streamize(xs)()
+    |GTcons(x1, rest) -> StrCons(x1, streamize(xs))
+
 
 let rec 
 gtree_streamize_dfs(xs: 'a gtree): 'a stream = 
-match xs with
-| GTnil -> StrCons(_, xs)
-| x1 :: xs -> StrCons(x1, xs);;
+fun() -> streamize(gtree_dfs([xs])(fchildren))()
+
+
+
+let rec
+gtree_bfs
+( nxs
+: 'b list)
+( fchildren
+: 'b -> 'b list): 'b stream = fun() ->
+(
+match nxs with
+  [] -> StrNil
+| nx1 :: nxs ->
+  StrCons(nx1, gtree_bfs(nxs @ fchildren(nx1))(fchildren))
+)
+;;
+
+let rec 
+gtree_streamize_bfs(xs: 'a gtree): 'a stream = 
+fun() -> streamize(gtree_bfs([xs])(fchildren))()
+
+    
